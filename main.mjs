8be2dfe06d5429e5e6ca83be8cbf5e6454de6105
rassi0429 <taitokokoa+rassi@gmail.com>
2022-07-15 00:25:45 +0900

    feat: fs

--- main.mjs
+++ main.mjs
@@ -4,9 +4,13 @@ import fs from "fs/promises"
 const app = new express()
 app.listen(4001,() => console.log("Ok"))
 
+const data = JSON.parse(await fs.readFile("./data/access.json"))
+
 app.get("/",async (req,res) => {
-    const data = JSON.parse(await fs.readFile("./data/access.json"))
     data.count = data.count + 1
-    await fs.writeFile("./data/access.json",JSON.stringify(data))
     res.send((data.count + 1).toString())
 })
+
+setTimeout(async () => {
+    await fs.writeFile("./data/access.json",JSON.stringify(data))
+}, 60000)
